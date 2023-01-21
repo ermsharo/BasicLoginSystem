1555c0691bbc228e4e56ecaf2d1c4adacb8425eb
Emilio Haro <ermsharo@gmail.com>
2023-01-21 00:01:51 -0300

    formating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,24 +10,7 @@ import Board from "./components/Board/Board";
 function App() {
   return (
     <>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Dict app</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <link
-          href="http://fonts.cdnfonts.com/css/sedgwick-ave"
-          rel="stylesheet"
-        />
-        <link href="http://fonts.cdnfonts.com/css/helmida" rel="stylesheet" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto+Slab&family=Varela+Round&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
+      <Helmet></Helmet>
 
       <div>
         <BrowserRouter>
