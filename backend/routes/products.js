fda2dbe0a9e9f2bb5c3e1f66627fd6ac4943808c
Emilio Haro <ermsharo@gmail.com>
2023-01-22 07:59:12 -0300

    prettier changes

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,19 +1,14 @@
-
 const express = require("express");
 const router = express.Router();
 require("dotenv-safe").config();
 const fs = require("fs");
-const file_content = fs.readFileSync('assets/products.json');
+const middleware = require("../middleware/middleware");
+const file_content = fs.readFileSync("assets/products.json");
 const products = JSON.parse(file_content);
 
-
-router.get("/products", async (req, res) => {
-
-    return res.status(200).json(products);
-
-
+router.get("/products", middleware, async (req, res) => {
+  console.log("req id", req.userId);
+  return res.status(200).json(products);
 });
-
-
 
 module.exports = router;
