ff12c41dd786132036a5e0763c7257a3020515ef
Emilio Haro <ermsharo@gmail.com>
2023-01-22 00:24:23 -0300

    update our changes

---
+++ backend/routes/products.js
@@ -0,0 +1,18 @@
+const { Op } = require("sequelize");
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+require("dotenv-safe").config();
+
+
+
+router.get("/products", async (req, res) => {
+
+
+
+});
+
+
+
+module.exports = router;
