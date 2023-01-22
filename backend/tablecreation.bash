33de65efdcc24318a951d2c15691a1adb98dfc4a
Emilio Haro <ermsharo@gmail.com>
2023-01-22 16:06:13 -0300

    remove trash and add read-me

--- backend/tablecreation.bash
+++ backend/tablecreation.bash
@@ -1,6 +1,8 @@
 npx sequelize-cli model:generate --name User --attributes id:string,userName:string,email:string,password:string --force
 npx sequelize-cli model:generate --name Product --attributes id:string,name:string,price:string --force
 
+
+npm install --save-dev sequelize-cli
 npx sequelize-cli db:migrate
 
 node index.js
--- runApp.bash
+++
@@ -1,4 +0,0 @@
-docker compose up
-nodemon backend/index.js
-cd frontend/my-app
-npm start 
